cff9192991132d1f137f68863e393e99a65a5e41
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2026-02-28 22:42:55 +0100

    feat: define core entities for Client and Revision

---
+++ manager-app/src/renderer/src/core/entities/Clients.ts
@@ -0,0 +1,14 @@
+export interface Cliente {
+    readonly id: string;
+    nombre: string;
+    email: string;
+    notas?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+/**
+ * Type para la creación de un nuevo cliente
+ * Omit para quitar los campos automáticos
+ */
+export type CreateClienteInput = Omit<Cliente, 'id' | 'createdAt' | 'updatedAt'>;
